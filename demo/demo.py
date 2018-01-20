c290b8bfee211ccad9fb09c769efaadbcb90e89a
Steven Spinner <stevenmspin1003@gmail.com>
2018-01-20 16:46:45 -0500

    DEMO: Allow more ports to be used

--- demo/demo.py
+++ demo/demo.py
@@ -2,15 +2,30 @@ from flask import Flask, request, render_template, redirect
 import os
 import sys
 import time
+import docker
+
+client = docker.from_env()
 
 app = Flask(__name__)
+ports = [0] * 1000
 
 @app.route('/', methods=["GET", "POST"])
 def button():
     if request.method == "POST":
-        os.system("sudo docker run -itd -p 5000:5000 grounder ./root/start.sh 0.0.0.0 5000")
-        time.sleep(1)
-        return redirect("http://ec2-34-238-117-47.compute-1.amazonaws.com:5000")
+    	port = 5000
+
+    	#Search for an unused port
+    	for i in range(5000,6000):
+    		if ports[i-5000] == 0:
+    			port = i
+    			break
+
+        # Launch the docker container
+        ports[port-5000] = client.containers.run("grounder", detach=True, command="./root/start.sh 0.0.0.0 " + str(port), ports={str(port) + '/tcp': str(port)})
+        time.sleep(1.5)
+
+        # Redirect the user to the newly launched docker container
+        return redirect("http://ec2-34-238-117-47.compute-1.amazonaws.com:" + str(port))
     return render_template("button.html")
 
 if __name__ == '__main__':
