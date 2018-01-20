eed554c44c23f3bfc52ca4000130061f49eb4636
Kevin Allen <kma1660@gmail.com>
2018-01-20 12:10:06 -0500

    add api interface for img view

--- static/js/app.js
+++ static/js/app.js
@@ -4,34 +4,62 @@ app.config(function($routeProvider) {
   .when("/", {
       templateUrl: "/html/home.html"
   })
-  .when("/img/:img*", {
+  .when("/img:img*", {
       templateUrl: "/html/img.html",
       controller: "imgCtrl"
   })
-  .when("/dir/:dir*", {
+  .when("/img", { redirectTo: "/img/" })
+  .when("/dir:dir*", {
       templateUrl: "/html/dir.html",
       controller: "dirCtrl"
   })
-  .when("/template/:template*", {
+  .when("/dir", {redirectTo: "/dir/" })
+  .when("/template:template*", {
       templateUrl: "/html/template.html",
       controller: "templateCtrl"
   })
+  .when("/template", {redirectTo: "/template/"})
   .otherwise({
       templateUrl: "/html/404.html"
   });
 });
 
-app.controller("imgCtrl", function($scope, $routeParams) {
-    console.log("img");
-    console.log($routeParams.img);
+app.controller("imgCtrl", function($scope, $routeParams, $http) {
+    $scope.img = $routeParams.img;
+    $scope.img_url = "/api/img" + $scope.img
+    $scope.old_label = {}
+    $scope.label = {}
+
+    $http.get("/api/label" + $scope.img).then(
+    function success(res) {
+        $scope.old_label = res.data
+        $scope.label = $scope.old_label
+    },
+    function error(res) {
+        console.warn('could not get label', res.status, res.data)
+    })
+
+    $scope.save = function() {
+        $http.post("/api/label" + $scope.img, $scope.label).then(
+        function success(res) {
+            console.log("Saved");
+        },
+        function error(res) {
+            console.warn("could not save label", res.status, res.data)
+        })
+    }
+
+    $scope.reset = function () {
+        $scope.label = $scope.old_label
+    }
 });
 
-app.controller("dirCtrl", function($scope, $routeParams) {
+app.controller("dirCtrl", function($scope, $routeParams, $http) {
     console.log("dir");
     console.log($routeParams.dir);
 });
 
-app.controller("templateCtrl", function($scope, $routeParams) {
+app.controller("templateCtrl", function($scope, $routeParams, $http) {
     console.log("template");
     console.log($routeParams.template);
 });
