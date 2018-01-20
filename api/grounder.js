d7fa304105790062042ec2373ba10bfb07540800
Nicholas Suhlman <grymestone@ufl.edu>
2018-01-20 15:24:26 -0500

    FLASK: Delete garbage files

--- api/grounder.js
+++
@@ -1,6 +0,0 @@
-exports.handleRequest = function (requestData, callback) { 
-	var responseData = {received_as_input: requestData}; 
-	callback(null, responseData);
-}
-
-
--- api/index.js
+++
@@ -1,6 +0,0 @@
-var package = require(“./package.json”);
-var grounder = require(“./lib/grounder.js”);
-console.log(“loaded ” + package.name + “, version ” + package.version);
-exports.handler = function (event, context) {
-grounder.handleRequest(event, context.done);
-}
