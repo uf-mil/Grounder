32df400b47adbab6aafee11b7106a8df71196491
Steven Spinner <stevenmspin1003@gmail.com>
2018-01-20 15:43:06 -0500

    DEMO: Add demo files

---
+++ demo/demo.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, redirect                                               │
+import os                                                                                                 │
+import sys                                                                                                │
+                                                                                                          │
+app = Flask(__name__)                                                                                     │
+                                                                                                          │
+ButtonPressed = 0                                                                                         │
+@app.route('/', methods=["GET", "POST"])
+def button():                                                                                             │
+    if request.method == "POST":                                                                          │
+        os.system("sudo docker run -itd -p 5000:5000 grounder ./root/start.sh 0.0.0.0 5000")
+        return redirect("http://ec2-34-238-117-47.compute-1.amazonaws.com:5000")                          │
+    return render_template("button.html")                                                                 │
+                                                                                                          │
+if __name__ == '__main__':                                                                                │
+     if len(sys.argv) == 3:                                                                               │
+        app.run(debug=True,host=sys.argv[1],port=int(sys.argv[2]))                                        │
+     else:                                                                                                │
+        print("Usage: demo.py <ip> <port>")   
